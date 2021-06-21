1fd0325790ae599e67246c9767d4afc24ba8df0a
Samy Muhammad <samy.m.reda@gmail.com>
2021-06-21 23:10:59 +0200

    writing relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,4 +17,19 @@ class Post extends Model
         'user_id',
         'category_id',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'tag_post');
+    }
 }
